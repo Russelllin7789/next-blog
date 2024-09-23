86a34d2e5f332ba4fe34dd43f408ee1a81d72d58
Russell Lin <superbr7789@gmail.com>
2024-09-23 19:22:14 +0800

    add themeToggle and authLinks component

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./authLinks.module.css";
+
+const AuthLinks = () => {
+  return (
+    <div>
+      <div className={styles.container}>AuthLinks</div>
+    </div>
+  );
+};
+
+export default AuthLinks;
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./themeToggle.module.css";
+
+const ThemeToggle = () => {
+  return (
+    <div>
+      <div className={styles.container}>ThemeToggle</div>
+    </div>
+  );
+};
+
+export default ThemeToggle;
