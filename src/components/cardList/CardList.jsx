53c0861a598e2018e3fad3d4c22fbe9ef5690652
Russell Lin <superbr7789@gmail.com>
2024-09-23 18:53:29 +0800

    fulfill basic structure for whole home page

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./cardList.module.css";
+import Pagination from "@/components/pagination/Pagination";
 
 const CardList = () => {
   return (
     <>
       <div className={styles.container}>CardList</div>
+      <Pagination />
     </>
   );
 };
